21047ce61f422dbd84015328e87e87e1c3113f59
Alex Javier <alexavier1989@gmail.com>
2025-01-05 00:37:54 -0500

    feat: Tasks Part 1 done. Examples of tradicional functions and array functions

--- js/index.js
+++ js/index.js
@@ -1,4 +1,32 @@
-// Prevent us from attempting to use variables
-// that are not declared
 "use strict"
 
+// A function to calculate the factorial of a number.
+// function factorial(n) {
+//     if (n === 0) {
+//         return 1;
+//     } else {
+//         return n * factorial( n - 1);
+//     }
+// }
+// console.log(factorial(8)); // Output: 40320
+
+
+// Using the filter array method to extract even numbers from an array.
+// function findEvens(arr) {
+//     return arr.filter((num) => num % 2 === 0);
+// }
+// console.log(findEvens([1, 2, 3, 4, 5, 6])); // Output: [2, 4, 6]
+
+
+// Using the reduce array method to calculate the sum of numbers in an array.
+// function sumArray(arr) {
+//     return arr.reduce((acc, curr) => acc + curr, 0);
+//   }
+  
+//   console.log(sumArray([1, 2, 3, 4, 5, 6])); // Output: 21
+
+
+// An array functions in a single line to check if a number is positive.
+// const isPositive = (num) => num > 0;
+// console.log(isPositive(9));  // Output: true
+// console.log(isPositive(-4)); // Output: false
